6ac73581dda65c6816f4aa2c862cb731a88989f0
mmedojevicbg <mmedojevicbg@gmail.com>
2017-01-26 14:22:00 +0100

    foreign keys

--- migrations/m170126_123350_alter_all_foreign_keys.php
+++ migrations/m170126_123350_alter_all_foreign_keys.php
@@ -6,11 +6,11 @@ class m170126_123350_alter_all_foreign_keys extends Migration
 {
     public function up()
     {
+        $this->addForeignKey('pje_execution_fk1', 'pje_execution', 'job_id', 'pje_job', 'id', 'cascade', 'cascade');
         $this->addForeignKey('pje_job_step_fk1', 'pje_job_step', 'job_id', 'pje_job', 'id', 'cascade', 'cascade');
         $this->addForeignKey('pje_job_step_fk2', 'pje_job_step', 'step_id', 'pje_step', 'id', 'cascade', 'cascade');
         $this->addForeignKey('pje_execution_step_fk1', 'pje_execution_step', 'execution_id', 'pje_execution', 'id', 'cascade', 'cascade');
         $this->addForeignKey('pje_execution_step_fk2', 'pje_execution_step', 'job_step_id', 'pje_job_step', 'id', 'cascade', 'cascade');
-        $this->addForeignKey('pje_execution_fk1', 'pje_execution', 'job_id', 'pje_job', 'id', 'cascade', 'cascade');
         $this->addForeignKey('pje_job_step_param_fk1', 'pje_job_step_param', 'job_step_id', 'pje_job_step', 'id', 'cascade', 'cascade');
         $this->addForeignKey('pje_notification_fk1', 'pje_notification', 'execution_id', 'pje_execution', 'id', 'cascade', 'cascade');
     }
